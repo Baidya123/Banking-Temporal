35798234b9497ee9aaacb52f2f1255c2922ec2c7
Biswajit Baidya <biswojitbaidya@gmail.com>
2022-02-24 20:54:45 +0530

    Updated application.properties

--- src/main/java/com/truist/bankingtemporal/config/ServiceConfig.java
+++ src/main/java/com/truist/bankingtemporal/config/ServiceConfig.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties("my.service.url")
+@ConfigurationProperties("truist.service-url")
 @Getter @Setter
 public class ServiceConfig {
     private String debit;
