68c0737b356b8a08b6ea4b43856998cb20164557
Baidya123 <biswojitbaidya@gmail.com>
2022-03-09 14:13:22 +0530

    Delete src directory

--- src/main/java/com/example/DebitMicroservice/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.DebitMicroservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.DebitMicroservice.model.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
-
-}
--- src/main/java/com/example/DebitMicroservice/utils/GlobalErrorHandler.java
+++
@@ -1,5 +0,0 @@
-package com.example.DebitMicroservice.utils;
-
-public class GlobalErrorHandler {
-
-}
